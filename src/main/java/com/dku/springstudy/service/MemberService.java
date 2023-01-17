c0e5106294ea9d0cb6a15b182ff2672928ac73a7
SeoJiHyeon <sgh9702@naver.com>
2023-01-17 11:05:23 +0900

    [feat] : 회원 서비스 구현

---
+++ src/main/java/com/dku/springstudy/service/MemberService.java
@@ -0,0 +1,34 @@
+package com.dku.springstudy.service;
+
+import com.dku.springstudy.domain.Member;
+import com.dku.springstudy.repository.MemberRepository;
+import com.dku.springstudy.repository.MemoryMemberRepository;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+        validateDuplicateMember(member); // method 로 뽑기 : control + t
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalStateException("이미 존재하는 회원입니다.");
+                        });
+    }
+}
