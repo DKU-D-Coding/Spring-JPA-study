dd3825d66bbf8ba982d0ba73140b1a0f80b596c6
SeoJiHyeon <sgh9702@naver.com>
2023-01-17 17:19:18 +0900

    [feat] : jdbc 레포지토리 구현

--- src/main/java/com/dku/springstudy/SpringConfig.java
+++ src/main/java/com/dku/springstudy/SpringConfig.java
@@ -1,13 +1,22 @@
 package com.dku.springstudy;
 
+import com.dku.springstudy.repository.JdbcMemberRepository;
 import com.dku.springstudy.repository.MemberRepository;
 import com.dku.springstudy.repository.MemoryMemberRepository;
 import com.dku.springstudy.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
