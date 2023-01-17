1a6bd44a9799bd54d05c8035fb4812125ac9b0a3
SeoJiHyeon <sgh9702@naver.com>
2023-01-17 11:34:03 +0900

    [feat] : 회원 서비스 테스트코드 구현, 의존성 주입고려

--- src/main/java/com/dku/springstudy/service/MemberService.java
+++ src/main/java/com/dku/springstudy/service/MemberService.java
@@ -9,7 +9,11 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     public Long join(Member member) {
         validateDuplicateMember(member); // method 로 뽑기 : control + t
