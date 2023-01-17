79e80dc0fe9c261a824af66b0f50347d99523f67
SeoJiHyeon <sgh9702@naver.com>
2023-01-17 12:46:55 +0900

    [feat] : 회원 웹기능(홈화면, 등록, 조회)

---
+++ src/main/java/com/dku/springstudy/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package com.dku.springstudy.controller;
+
+public class MemberForm {
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
