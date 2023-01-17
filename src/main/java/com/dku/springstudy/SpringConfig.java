562848af36ac127c26ece6e45643199a1af77dcc
SeoJiHyeon <sgh9702@naver.com>
2023-01-17 21:03:40 +0900

    [feat] : jpa 레포지토리, 엔티티매핑, 트랜잭션 설정

--- src/main/java/com/dku/springstudy/SpringConfig.java
+++ src/main/java/com/dku/springstudy/SpringConfig.java
@@ -1,23 +1,27 @@
 package com.dku.springstudy;
 
-import com.dku.springstudy.repository.JdbcMemberRepository;
-import com.dku.springstudy.repository.JdbcTemplateMemberRepository;
-import com.dku.springstudy.repository.MemberRepository;
-import com.dku.springstudy.repository.MemoryMemberRepository;
+import com.dku.springstudy.repository.*;
 import com.dku.springstudy.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
+    private EntityManager em;
 
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
+
+    //private final DataSource dataSource;
+
+    //public SpringConfig(DataSource dataSource) {
+        //this.dataSource = dataSource;
+   // }
 
     @Bean
     public MemberService memberService(){
@@ -28,6 +32,7 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
--- src/main/java/com/dku/springstudy/service/MemberService.java
+++ src/main/java/com/dku/springstudy/service/MemberService.java
@@ -5,11 +5,13 @@ import com.dku.springstudy.repository.MemberRepository;
 import com.dku.springstudy.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
