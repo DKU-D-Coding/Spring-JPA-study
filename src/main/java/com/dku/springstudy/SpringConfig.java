5339920eaad7759a2f0a2a84ddf625ec35664595
SeoJiHyeon <sgh9702@naver.com>
2023-01-17 21:26:27 +0900

    [feat] : spring data jpa

--- src/main/java/com/dku/springstudy/SpringConfig.java
+++ src/main/java/com/dku/springstudy/SpringConfig.java
@@ -2,6 +2,7 @@ package com.dku.springstudy;
 
 import com.dku.springstudy.repository.*;
 import com.dku.springstudy.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,28 +12,38 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    @Autowired //생략가능
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
-    //private final DataSource dataSource;
-
-    //public SpringConfig(DataSource dataSource) {
-        //this.dataSource = dataSource;
-   // }
+    /**
+     * private EntityManager em;
+     * <p>
+     * public SpringConfig(EntityManager em) {
+     * this.em = em;
+     * }
+     * <p>
+     * private final DataSource dataSource;
+     * <p>
+     * public SpringConfig(DataSource dataSource) {
+     * this.dataSource = dataSource;
+     * }
+     */
 
     @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-        //return new MemoryMemberRepository();
-        //return new JdbcMemberRepository(dataSource);
-        //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+    /**
+     @Bean public MemberRepository memberRepository(){
+     //return new MemoryMemberRepository();
+     //return new JdbcMemberRepository(dataSource);
+     //return new JdbcTemplateMemberRepository(dataSource);
+     return new JpaMemberRepository(em);
+     }
+     */
 }
---
+++ src/main/java/com/dku/springstudy/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,14 @@
+package com.dku.springstudy.repository;
+
+import com.dku.springstudy.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
