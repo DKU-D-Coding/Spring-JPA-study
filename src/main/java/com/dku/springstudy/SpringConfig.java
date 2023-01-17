b45495f84bf37da86da69702fef8d59b323aa3fc
SeoJiHyeon <sgh9702@naver.com>
2023-01-17 17:54:42 +0900

    [feat] : 스프링 JdbcTemplate 회원 리포지토리 구현

--- src/main/java/com/dku/springstudy/SpringConfig.java
+++ src/main/java/com/dku/springstudy/SpringConfig.java
@@ -1,6 +1,7 @@
 package com.dku.springstudy;
 
 import com.dku.springstudy.repository.JdbcMemberRepository;
+import com.dku.springstudy.repository.JdbcTemplateMemberRepository;
 import com.dku.springstudy.repository.MemberRepository;
 import com.dku.springstudy.repository.MemoryMemberRepository;
 import com.dku.springstudy.service.MemberService;
@@ -26,6 +27,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
