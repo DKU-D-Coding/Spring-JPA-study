12b4691342b1ed18e903d9a61f0b063e7d411d66
sujeong11 <qalzm7351@gmail.com>
2023-01-27 21:30:11 +0900

    feat: 기본 Security 설정 추가

---
+++ src/main/java/com/dku/springstudy/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.dku.springstudy.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/api/sign-up", "/api/login").permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+}
