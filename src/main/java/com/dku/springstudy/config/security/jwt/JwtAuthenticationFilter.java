a4704e37399522c5681e8ec6770b02a18bbe8868
SeoJiHyeon <sgh9702@naver.com>
2023-01-27 14:29:55 +0900

    [feat]: JWT 토큰 생성 및 JWT 인증 필터 구현

---
+++ src/main/java/com/dku/springstudy/config/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,97 @@
+package com.dku.springstudy.config.security.jwt;
+
+import com.dku.springstudy.dto.ResponseDTO;
+import com.dku.springstudy.exception.Message;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            // 요청에서 토큰 가져오기
+            String token = parseBearerToken(request);
+            log.info("Filter is running...");
+
+            if (token != null && !token.equalsIgnoreCase("null")) {
+                // userId 가져오기. 위조된 경우 예외 처리된다.
+                Claims claims = jwtProvider.validateAndGetUserId(token);
+                String userId = claims.getSubject();
+                String userRole = (String) claims.get("ROLE");
+                Date expiration = claims.getExpiration();
+                log.info("Authenticated user ID : " + userId);
+                log.info("Authenticated expiration : " + expiration);
+
+                AbstractAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userId, // 인증된 사용자의 정보. 문자열이 아니어도 아무것이나 넣을 수 있다. 보통 UserDetails 오브젝트를 넣음
+                        null,
+                        AuthorityUtils.createAuthorityList(userRole));
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+                securityContext.setAuthentication(authentication);
+                SecurityContextHolder.setContext(securityContext);
+            }
+
+            filterChain.doFilter(request, response);
+        } catch (ExpiredJwtException e) {
+            logger.error("Could not set user authentication in security context {}", e);
+            jwtExceptionHandler(response, Message.JWT_TOKEN_EXPIRED);
+        } catch (UnsupportedJwtException e) {
+            logger.error("Could not set user authentication in security context {}", e);
+            jwtExceptionHandler(response, Message.JWT_UNSUPPORTED);
+        } catch (MalformedJwtException e) {
+            logger.error("Could not set user authentication in security context {}", e);
+            jwtExceptionHandler(response, Message.JWT_MALFORMED);
+        } catch (SignatureException e) {
+            logger.error("Could not set user authentication in security context {}", e);
+            jwtExceptionHandler(response, Message.JWT_SIGNATURE);
+        } catch (IllegalArgumentException e) {
+            logger.error("Could not set user authentication in security context {}", e);
+            jwtExceptionHandler(response, Message.JWT_ILLEGAL_ARGUMENT);
+        }
+    }
+    private String parseBearerToken(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    private void jwtExceptionHandler(HttpServletResponse response, String message) throws IOException{
+        ResponseDTO<String> dto = new ResponseDTO<>(HttpStatus.UNAUTHORIZED.value(), message);
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        objectMapper.writeValue(response.getWriter(), dto);
+    }
+}
---
+++ src/main/java/com/dku/springstudy/config/security/jwt/JwtProvider.java
@@ -0,0 +1,77 @@
+package com.dku.springstudy.config.security.jwt;
+
+import com.dku.springstudy.dto.TokenResponse;
+import com.dku.springstudy.dto.UserResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+
+    private final ObjectMapper objectMapper;
+
+    @Value("${jwt.key}")
+    private String key;
+
+    @Value("${jwt.live.atk}")
+    private Long atkLive;
+
+    @PostConstruct
+    protected void init() {
+        key = Base64.getEncoder().encodeToString(key.getBytes());
+    }
+
+    public TokenResponse createTokensByLogin(UserResponse userResponse) throws JsonProcessingException {
+        Subject atkSubject = Subject.atk(
+                userResponse.getUserId(),
+                userResponse.getEmail(),
+                userResponse.getNickname());
+        String atk = createToken(atkSubject, atkLive);
+        return new TokenResponse(atk, null);
+    }
+
+    private String createToken(Subject subject, Long tokenLive) throws JsonProcessingException {
+        String subjectStr = objectMapper.writeValueAsString(subject);
+        Claims claims = Jwts.claims().setSubject(subjectStr);
+        Date date = new Date();
+        return Jwts.builder()
+                .setSubject(claims.getId())
+                .setClaims(claims)
+                .setIssuedAt(date)
+                .setExpiration(new Date(date.getTime() + tokenLive))
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact();
+    }
+
+
+    public Claims validateAndGetUserId(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims; // 사용자의 아이디 리턴
+    }
+
+    public boolean isValidToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(key)
+                    .parseClaimsJws(token)
+                    .getBody();
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+}
