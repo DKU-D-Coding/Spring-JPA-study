305e8e08cdf8fa87ee7192a33481a488bf9d86ab
sujeong11 <qalzm7351@gmail.com>
2023-01-28 00:08:39 +0900

    feat: User 레포지토리 생성

---
+++ src/main/java/com/dku/springstudy/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.dku.springstudy.repository;
+
+import com.dku.springstudy.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
