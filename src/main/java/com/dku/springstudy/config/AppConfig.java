fde437c8a54cf35baeee36ba58ec230746c699b0
SeoJiHyeon <sgh9702@naver.com>
2023-01-27 11:39:19 +0900

    [feat]: SecurityFilterChain 수동으로 스프링 컨테이너 등록

---
+++ src/main/java/com/dku/springstudy/config/AppConfig.java
@@ -0,0 +1,41 @@
+package com.dku.springstudy.config;
+
+import com.dku.springstudy.config.security.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class AppConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/account/sign-up", "/account/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterAfter(jwtAuthenticationFilter, CorsFilter.class)
+                .build();
+    }
+}
