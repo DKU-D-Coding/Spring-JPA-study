a7cd823588ae6ec2722ecc9541fd30cd2b40ac7f
gutanbug <gutanbug53@gmail.com>
2023-01-29 22:45:34 +0900

    JWT와 Security를 이용한 로그인과 회원가입 구현

---
+++ src/main/java/com/cha/carrotApi/CarrotApiApplication.java
@@ -0,0 +1,15 @@
+package com.cha.carrotApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class CarrotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cha/carrotApi/service/CustomUserDetailsService.java
@@ -0,0 +1,19 @@
+package com.cha.carrotApi.service;
+
+import com.cha.carrotApi.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+    }
+}
