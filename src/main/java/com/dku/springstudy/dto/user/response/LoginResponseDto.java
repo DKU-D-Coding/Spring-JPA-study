ad4490a21421298504e097e2ef98e23324b3a261
sujeong11 <qalzm7351@gmail.com>
2023-02-02 05:31:16 +0900

    docs: 회원 API swagger 문서화 작성

--- src/main/java/com/dku/springstudy/dto/user/response/LoginResponseDto.java
+++ src/main/java/com/dku/springstudy/dto/user/response/LoginResponseDto.java
@@ -1,5 +1,6 @@
 package com.dku.springstudy.dto.user.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,7 +9,9 @@ import lombok.Getter;
 @Getter
 public class LoginResponseDto {
 
+    @Schema(description = "로그인 성공 시 발급되는 토큰")
     private String accessToken;
+    @Schema(description = "로그인 성공 시 발급되는 재발급토큰")
     private String refreshToken;
 
     public static LoginResponseDto of(String accessToken, String refreshToken) {
--- src/main/java/com/dku/springstudy/dto/user/response/SignUpResponseDto.java
+++ src/main/java/com/dku/springstudy/dto/user/response/SignUpResponseDto.java
@@ -1,5 +1,6 @@
 package com.dku.springstudy.dto.user.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,6 +9,7 @@ import lombok.Getter;
 @Getter
 public class SignUpResponseDto {
 
+    @Schema(example = "12", description = "회원가입이 완료된 사용자의 PK")
     private Long id;
 
     public static SignUpResponseDto of(Long id) {
