4bd7a504d84d077abcbaf7461e4564be03d3107c
YunTaeHyeon <t05h19@naver.com>
2023-02-03 18:11:56 +0900

    MemberItemRepository 수정

--- src/main/java/com/project/carrot/repository/MemberItemRepository.java
+++ src/main/java/com/project/carrot/repository/MemberItemRepository.java
@@ -3,11 +3,15 @@ package com.project.carrot.repository;
 import com.project.carrot.domain.Member;
 import com.project.carrot.domain.MemberItem;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberItemRepository {
     MemberItem save(MemberItem memberItem);
-    Optional<MemberItem> findById(int Id);
-    Optional<MemberItem> findByName(String name);
-    Optional<MemberItem> findByPass(String pass);
+    List<MemberItem> findAll();
+    List<MemberItem> findByCategory(String category);
+    Optional<MemberItem> findByTitle(String title);
+    Optional<MemberItem> findByContent(String content);
+
+
 }
