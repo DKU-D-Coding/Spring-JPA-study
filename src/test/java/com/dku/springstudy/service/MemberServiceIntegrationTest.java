fe7a0dd7a0ee6ab726e4790f50ca9d9d78c8e263
Cho SangHyun <98shcho@naver.com>
2023-02-06 14:41:42 +0900

    test: 회원가입테스트 리팩토링

--- src/test/java/com/dku/springstudy/service/MemberServiceIntegrationTest.java
+++ src/test/java/com/dku/springstudy/service/MemberServiceIntegrationTest.java
@@ -4,9 +4,11 @@ import com.dku.springstudy.model.Member;
 import com.dku.springstudy.model.Role;
 import com.dku.springstudy.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,34 +21,52 @@ class MemberServiceIntegrationTest {
     MemberService memberService;
     @Autowired
     MemberRepository memberRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
-    @DisplayName("회원가입 테스트")
-    @Test
-    void joinTest() {
-        Member member = new Member();
-        member.setEmail("testets123@google.com");
-        member.setPassword("123123");
-        member.setNickname("testestrqe");
-        member.setRole(Role.USER);
+    @Nested
+    class SignupTest {
+        @DisplayName("회원가입 테스트")
+        @Test
+        void joinTest() {
+            Member member = new Member();
+            member.setEmail("testets123@google.com");
+            member.setPassword(passwordEncoder.encode("123123"));
+            member.setName("김김김");
+            member.setPhone("01000121234");
+            member.setNickname("testestrqe");
+            member.setRole(Role.USER);
 
-        String memberEmail = memberService.join(member);
+            String memberEmail = memberService.join(member);
 
-        Member foundMember = memberRepository.findByEmail(memberEmail).get();
-        System.out.println(foundMember.getEmail());
-        System.out.println(memberEmail);
-        assertThat(foundMember.getEmail()).isEqualTo(memberEmail);
-    }
+            Member foundMember = memberRepository.findByEmail(memberEmail).get();
+            System.out.println(foundMember.getEmail());
+            System.out.println(memberEmail);
+            assertThat(foundMember.getEmail()).isEqualTo(memberEmail);
+        }
 
-    @DisplayName("이메일 중복되면 오류가 난다")
-    @Test
-    void joinTestByExistingEmail() {
-        Member member = new Member();
-        member.setEmail("dku@google.com");
-        member.setPassword("123123");
-        member.setNickname("testestrqe");
-        member.setRole(Role.USER);
+        @DisplayName("이메일 중복되면 오류가 난다")
+        @Test
+        void joinTestByExistingEmail() {
+            Member member = new Member();
+            member.setEmail("testets123@google.com");
+            member.setPassword(passwordEncoder.encode("123123"));
+            member.setName("김김김");
+            member.setPhone("01000121234");
+            member.setNickname("testestrqe");
+            member.setRole(Role.USER);
+            memberService.join(member);
 
-        assertThatThrownBy(() -> memberService.join(member))
-                .isInstanceOf(IllegalArgumentException.class);
+            Member member2 = new Member();
+            member2.setEmail("testets123@google.com");
+            member2.setPassword(passwordEncoder.encode("123123"));
+            member2.setName("박박박");
+            member2.setPhone("01012345678");
+            member2.setNickname("testsrqwqe");
+            member2.setRole(Role.USER);
+
+            assertThatThrownBy(() -> memberService.join(member2))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }
