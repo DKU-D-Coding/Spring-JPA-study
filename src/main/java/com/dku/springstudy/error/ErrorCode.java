1a5a39ffdd914cd293e40b7f32fed6e208f2b327
최재민 <panzzang518@naver.com>
2023-02-08 10:01:09 +0900

    Fix: ErrorCode enum으로 변경

--- src/main/java/com/dku/springstudy/error/ErrorCode.java
+++ src/main/java/com/dku/springstudy/error/ErrorCode.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 @Getter
-public class ErrorCode {
+public enum ErrorCode {
+    ALREADY_EXIST(400, "G001", "이미 존재합니다"),
+    NOT_EXIST(400, "G002", "존재하지 않습니다");
 
     private final int httpStatus;
     private final String code;
