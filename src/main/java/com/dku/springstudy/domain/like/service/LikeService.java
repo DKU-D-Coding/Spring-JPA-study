0f53c109029a5837c0a77cd831fe45179523e00b
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2023-02-13 00:31:35 +0900

    feat: LikeService 추가

---
+++ src/main/java/com/dku/springstudy/domain/like/service/LikeService.java
@@ -0,0 +1,12 @@
+package com.dku.springstudy.domain.like.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class LikeService {
+
+}
