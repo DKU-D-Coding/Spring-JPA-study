048b279bc94b0c40026e9704c27dd277a1e75313
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2023-02-12 20:45:32 +0900

    refactor: 상품 등록 기능 추가

--- src/main/java/com/dku/springstudy/domain/product/service/ProductService.java
+++ src/main/java/com/dku/springstudy/domain/product/service/ProductService.java
@@ -1,6 +1,10 @@
 package com.dku.springstudy.domain.product.service;
 
+import com.dku.springstudy.domain.product.Product;
+import com.dku.springstudy.domain.product.dto.ProductRegisterRequestDTO;
 import com.dku.springstudy.domain.product.repository.ProductRepository;
+import com.dku.springstudy.domain.user.User;
+import com.dku.springstudy.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,4 +14,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+
+    @Transactional
+    public Long save(ProductRegisterRequestDTO requestDTO, User user) {
+        Product product = requestDTO.toEntity(user);
+        productRepository.save(product);
+
+        return product.getId();
+    }
+
 }
