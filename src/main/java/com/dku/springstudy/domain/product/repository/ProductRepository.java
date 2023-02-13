d883f2af58f2cc8bf89b2eae51c1249cb170a7fc
이원규 <85729858+OneK-2@users.noreply.github.com>
2023-02-13 17:41:18 +0900

    feat: 상품조회 위한 메서드 추가

--- src/main/java/com/dku/springstudy/domain/product/repository/ProductRepository.java
+++ src/main/java/com/dku/springstudy/domain/product/repository/ProductRepository.java
@@ -3,5 +3,9 @@ package com.dku.springstudy.domain.product.repository;
 import com.dku.springstudy.domain.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<List<Product>> findProductByUserId(Long userId);
 }
